ef2dc9a1c33bafaa0f22c7c16cfa7cd8e90f581f
Marcusedlund88 <115147753+Marcusedlund88@users.noreply.github.com>
2022-12-14 13:00:42 +0100

    Update Render.java

--- src/main/java/se/nackademin/Window/Render.java
+++ src/main/java/se/nackademin/Window/Render.java
@@ -1,16 +1,28 @@
-package se.nackademin.Window;
+package main.java.se.nackademin.Window;
 
-import se.nackademin.game.GameComponent;
+import main.java.se.nackademin.GameEngine.GameComponent;
 
 import java.awt.*;
+import java.awt.image.DataBufferInt;
 
 public class Render {
-
-    private GameComponent c;
+    private Canvas canvas;
+    private int pixelWidth = 45;
+    private int pixelHeight = 45;
+    private int[] pixel;
 
     public Render(GameComponent c) {
 
         Graphics g = c.getWindow().getCanvas().getGraphics();
+        canvas = c.getWindow().getCanvas();
+        pixel = ((DataBufferInt)c.getWindow().getBufferedImage().getRaster().getDataBuffer()).getData();
+    }
+
+    public void paintPixel(int xPosition, int yPosition, int colorValue){
+        if(xPosition < 0 || xPosition > canvas.getWidth() || yPosition < 0 || yPosition > canvas.getHeight()){
+            return;
+        }
+        pixel[xPosition + yPosition * pixelWidth] = colorValue;
     }
     public void drawGrid(Graphics g, int noGrids, int width, int height){
 
@@ -23,10 +35,24 @@ public class Render {
             g.drawLine(0,i*height/noGrids,width, i*height/noGrids);
         }
     }
-    public void drawRectangle(Graphics g, int xPos, int yPos, int noGrids, int width, int height){
+    public void drawRectangle(int xPosition, int yPosition, int width, int height, int colorValue){
 
-        g.setColor(Color.BLACK);
-        g.fillRect(xPos,yPos,width/noGrids,height/noGrids);
+        if(xPosition < 0 || xPosition > canvas.getWidth() || yPosition < 0 || yPosition > canvas.getHeight()){
+            return;
+        }
+        else {
+            for(int i = 0; i < width; i++) {
+                for(int j = 0; j < height; j++) {
+                    paintPixel((xPosition +i),(yPosition+j),colorValue);
+                }
+            }
+        }
+        System.out.println("");
 
+    }
+    public void clearDrawing(){
+        for (int i = 0; i < pixel.length; i++) {
+            pixel[i] = 0;
+        }
     }
 }
